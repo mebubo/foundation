5a50e659fe1453c59a24ebb86a5957b41c98f1b5
Sergei Dolgov <dolgovs@gmail.com>
2019-08-31 22:02:32 +0200

    Snapshot

--- exercises/src/main/scala/exercises/function/FunctionExercises.scala
+++ exercises/src/main/scala/exercises/function/FunctionExercises.scala
@@ -25,31 +25,31 @@ object FunctionExercises extends FunctionToImpl {
   // 1a. Implement tripleVal such as it behaves in the same way as triple
   def triple(x: Int): Int = 3 * x
 
-  val tripleVal: Int => Int = (x: Int) => ???
+  val tripleVal: Int => Int = (x: Int) => 3 * x
 
   // 1b. Implement tripleList by reusing triple or tripleVal, what's the difference?
   // such as tripleList(List(1,2,3)) == List(3,6,9)
   // hint: you can use map from List
-  def tripleList(xs: List[Int]): List[Int] = ???
+  def tripleList(xs: List[Int]): List[Int] = xs.map(triple)
 
   // 1c. Implement tripleVal2 by transforming triple into a val
-  val tripleVal2: Int => Int = _ => ???
+  val tripleVal2: Int => Int = triple _
 
   // 1d. Implement move that increase or decrease an Int based on a Boolean flag
   // such as move(true ).apply(5) == 6
   // but     move(false).apply(5) == 4
   // note: move(true).apply(5) can be shorten to move(true)(5)
-  def move(increment: Boolean): Int => Int = ???
+  def move(increment: Boolean): Int => Int = x => if (increment) x + 1 else x - 1
 
   // 1e. Implement move2 and move3 by reusing move
   // what's the difference between the two?
-  val move2: (Boolean, Int) => Int = (_, _) => ???
+  val move2: (Boolean, Int) => Int = (b, x) => move(b)(x)
 
-  val move3: Boolean => Int => Int = _ => ???
+  val move3: Boolean => Int => Int = move _
 
   // 1f. Implement applyMany
   // such as applyMany(List(_ + 1, _ - 1, _ * 2))(10) == List(11, 9, 20)
-  def applyMany(xs: List[Int => Int]): Int => List[Int] = ???
+  def applyMany(xs: List[Int => Int]): Int => List[Int] = y => xs.map(_(y))
 
   ////////////////////////////
   // 2. polymorphic functions
@@ -58,20 +58,20 @@ object FunctionExercises extends FunctionToImpl {
   // 2a. Implement identity
   // such as identity(1) == 1
   //         identity("foo") == "foo"
-  def identity[A](x: A): A = ???
+  def identity[A](x: A): A = x
 
   // 2b. Implement const
   // such as const(5)("foo") == 5
   //         List(1,2,3).map(const(0)) == List(0,0,0)
-  def const[A, B](a: A)(b: B): A = ???
+  def const[A, B](a: A)(b: B): A = a
 
   // 2c. Transform identity into a function (val). See Eta expansion https://stackoverflow.com/a/39446986
-  // val idVal = ???
+  val idVal = identity _
 
   // 2d. Implement apply and apply2 which both call a function for an input
   // such as apply(5, (_: Int) + 1) == 6
   // what's the difference with apply2?
-  def apply[A, B](value: A, f: A => B): B = ???
+  def apply[A, B](value: A, f: A => B): B = f(value)
 
   def apply2[A, B](value: A)(f: A => B): B = ???
 
@@ -85,12 +85,12 @@ object FunctionExercises extends FunctionToImpl {
       p.copy(age = f(p.age))
     }
 
-  def setAge(value: Int): List[User] = ???
+  def setAge(value: Int): List[User] = updateAge(const(value))
 
   // 2f. implement getUsers which returns all users
   // such as getUsers == List(User("John", 26), User("Lisa", 5))
   // hint: use updateAge with one of the polymorphic function we just saw
-  def getUsers: List[User] = ???
+  def getUsers: List[User] = updateAge(identity)
 
   // 2g. Implement andThen and compose
   // such as
@@ -98,18 +98,18 @@ object FunctionExercises extends FunctionToImpl {
   // val inc   : Int => Int = _ + 1
   // compose(isEven, inc)(10) == false
   // andThen(inc, isEven)(10) == false
-  def andThen[A, B, C](f: A => B, g: B => C): A => C = ???
+  def andThen[A, B, C](f: A => B, g: B => C): A => C = a => g(f(a))
 
-  def compose[A, B, C](f: B => C, g: A => B): A => C = ???
+  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
 
   // 2h. Implement the function f(x) = 2 * x + 1 using inc, double with compose or andThen
   val inc: Int => Int    = x => x + 1
   val double: Int => Int = x => 2 * x
 
-  val doubleInc: Int => Int = identity // ???
+  val doubleInc: Int => Int = double andThen inc
 
   // 2i. Same for f(x) = 2 * (x + 1)
-  val incDouble: Int => Int = identity // ???
+  val incDouble: Int => Int = double compose inc
 
   // 2j. inc and double are a special case of function where the input and output type is the same.
   // These functions are called endofunctions.
@@ -124,13 +124,22 @@ object FunctionExercises extends FunctionToImpl {
 
   // 3a. Implement sumList using an imperative approach (while, for loop)
   // such as sumList(List(1,5,2)) == 8
-  def sumList(xs: List[Int]): Int = ???
+  def sumList(xs: List[Int]): Int = {
+    var r = 0
+    for {
+      x <- xs
+    } r += x
+    r
+  }
 
   // 3b. Use recursion to implement sumList2
   // does your implementation works with large list? e.g. sumList2(List.fill(1000000)(1))
   val largeList: List[Int] = List.fill(1000000)(1) // List(1,1,1,1, ...)
 
-  def sumList2(xs: List[Int]): Int = ???
+  def sumList2(xs: List[Int]): Int = xs match {
+    case Nil => 0
+    case x :: xs => x + sum(xs)
+  }
 
   ///////////////////////
   // GO BACK TO SLIDES
@@ -160,7 +169,7 @@ object FunctionExercises extends FunctionToImpl {
 
   // 3c. Implement multiply using foldLeft
   // such as multiply(List(3,2,4)) == 24
-  def multiply(xs: List[Int]): Int = ???
+  def multiply(xs: List[Int]): Int = foldLeft(xs, 1)(_ * _)
 
   // 3d. Implement filter using foldLeft
   // such as filter(List(1,2,3,4))(isEven) == List(2,4)
@@ -172,7 +181,11 @@ object FunctionExercises extends FunctionToImpl {
   // but     forAll(List(true, false, true)) == false
   // does your implementation terminate early? e.g. forAll(List(false, false, false)) does not go through the entire list
   // does your implementation works for large list? e.g. forAll(List.fill(1000000)(true))
-  def forAll(xs: List[Boolean]): Boolean = ???
+  @tailrec
+  def forAll(xs: List[Boolean]): Boolean = xs match {
+    case Nil => true
+    case head :: next => head && forAll(next)
+  }
 
   // 3f. Implement find using a recursion or loop
   // find returns the first element of a List that matches the predicate `p`
@@ -195,7 +208,7 @@ object FunctionExercises extends FunctionToImpl {
       case h :: t => f(h, foldRight(t, z)(f))
     }
 
-  def forAll2(xs: List[Boolean]): Boolean = ???
+  def forAll2(xs: List[Boolean]): Boolean = foldRight(xs, true)((h, t) => if (!h) false else t)
 
   def find2[A](xs: List[A])(p: A => Boolean): Option[A] = ???
 
@@ -217,21 +230,24 @@ object FunctionExercises extends FunctionToImpl {
   // 3j. does the commented function below compile? If yes, what happens when you call it
   // Search for General recursion
   // or https://www.quora.com/Whats-the-big-deal-about-recursion-without-a-terminating-condition
-  //  def foo: Int = foo
+  def foo: Int = foo
 
   ////////////////////////
   // 4. Pure functions
   ////////////////////////
 
   // 4a. is plus a pure function? why?
+  // yes
   def plus(a: Int, b: Int): Int = a + b
 
   // 4b. is div a pure function? why?
+  // no
   def div(a: Int, b: Int): Int =
     if (b == 0) sys.error("Cannot divide by 0")
     else a / b
 
   // 4c. is times2 a pure function? why?
+  // no
   var counterTimes2 = 0
   def times2(i: Int): Int = {
     counterTimes2 += 1
@@ -239,33 +255,40 @@ object FunctionExercises extends FunctionToImpl {
   }
 
   // 4d. is boolToInt a pure function? why?
+  // no
   def boolToInt(b: Boolean): Int =
     if (b) 5
     else Random.nextInt() / 2
 
   // 4e. is mapLookup a pure function? why?
+  // yes
   def mapLookup(map: Map[String, Int], key: String): Int =
     map(key)
 
   // 4f. is times3 a pure function? why?
+  // no
   def times3(i: Int): Int = {
     println("do something here") // could be a database access or http call
     i * 3
   }
 
   // 4g. is circleArea a pure function? why?
+  // yes
   val pi = 3.14
   def circleArea(radius: Double): Double =
     radius * radius * pi
 
   // 4h. is inc or inc_v2 a pure function? why?
+  // no
   def inc(xs: Array[Int]): Unit =
     for { i <- 0 to xs.length } xs(i) = xs(i) + 1
 
+  // no
   def inc_v2(xs: Array[Int]): Unit =
     for { i <- xs.indices } xs(i) = xs(i) + 1
 
   // 4i. is incAll a pure function? why?
+  // no
   def incAll(value: Any): Any = value match {
     case x: Int    => x + 1
     case x: Long   => x + 1
@@ -273,6 +296,7 @@ object FunctionExercises extends FunctionToImpl {
   }
 
   // 4j. is incAll_v2 a pure function? why?
+  // reflection
   def incAll_v2(value: Any): Any = value match {
     case x: Int    => x + 1
     case x: Long   => x + 1
@@ -281,6 +305,7 @@ object FunctionExercises extends FunctionToImpl {
   }
 
   // 4k. is sum a pure function? why?
+  // yes
   def sum(xs: List[Int]): Int = {
     var acc = 0
     xs.foreach(x => acc += x)
